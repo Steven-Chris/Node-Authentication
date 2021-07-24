9eff182aeb14e1226c5c59fccbfc36dd49158392
steven-chris <stevenchris240@gmail.com>
2021-07-24 15:32:19 +0530

    added: cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 require("dotenv").config({ path: "./config.env" });
 const express = require("express");
+const cors = require("cors");
 require("./db/mongoose");
+
 const userRouter = require("./routers/user");
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
